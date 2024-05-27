615c1dcc050283a9119ea83c743352fdeef049c4
jkawai35 <jkawai@ucsc.edu>
2024-05-27 13:51:37 -0700

    wave manager implemented

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -24,6 +24,7 @@ public class EnemyMovement : MonoBehaviour
             pathIndex++;
             if (pathIndex >= LevelManager.main.path.Length)
             {
+                EnemySpawner.onEnemyDestroy.Invoke();
                 Destroy(gameObject);
                 return;
             } 
